e4f7a7bb245a1f04906744d8f29dcd888f3fea44
rafi <rafihuseinbagaskara@gmail.com>
2024-03-14 10:04:22 +0700

    Updated
    Installing Nodemon, Adding Images, Adding style.css, Adding style.scss, Adding cars.html, Adding index.html. Modifying index.js, Modifying package.json

--- server/index.js
+++ server/index.js
@@ -1,31 +1,24 @@
-// core package -> Package bawaan dari node.js
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-
-// third party package
 const url = require("url");
-
-const PUBLIC_DIR = path.join(__dirname, "../public");
+const PUBLIC_DIRECTORY = path.join(__dirname, "../public");
 const PORT = 8000;
 
-const server = (req, res) => {
-    // localhost:8000
-    if (req.url === '/'){
-        // res.end('INI DEFAULT SERVER');
-        req.url = "index.example.html";
-    } 
-    // Localhost:8000/search
-    else if (req.url === '/search'){
-        res.end('INI HALAMAN SEARCH')
-    } else {
-        res.end('Page Not Found 404')
-    }
 
+const server = (req, res) => {
+
+    if (req.url === "/") {
+        req.url = "/index.html"
+    } else if (req.url === '/cars') {
+        req.url = '/cars.html'
+    } else {
+        req.url = req.url;
+    }
     const parseURL = url.parse(req.url);
     const pathName = `${parseURL.pathname}`;
     const extension = path.parse(pathName).ext;
-    const absolutePath = path.join(PUBLIC_DIR, pathName);
+    const absolutePath = path.join(PUBLIC_DIRECTORY, pathName);
 
     const contentTypes = {
         ".css": "text/css",
@@ -44,8 +37,7 @@ const server = (req, res) => {
             res.end(data);
         }
     });
-    
-};
+}
 
-http.createServer(server).listen(PORT)
+http.createServer(server).listen(PORT);
 console.log(`Server is running ... PORT : localhost:${PORT}`);
